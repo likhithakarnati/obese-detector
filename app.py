65b0273dfc9d16e59dcedbeb9f8faf8e5702d404
likhithakarnati <141921716+likhithakarnati@users.noreply.github.com>
2023-08-10 17:26:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ height = st.slider('What is your height?', 0, 200, 25)
 model = joblib.load('model_new')
 result = model.predict([[weight,height]])[0]
 if st.button("Predict"):
-  st.write("You are {result}")
+  st.write(f"You are {result}")
